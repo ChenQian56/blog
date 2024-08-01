e1f2ee6cd324e1f996254aaaf9ec1b15b2c43a1f
ChenQian56 <18301786727@qq.com>
2024-08-01 14:02:49 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,2 +1,16 @@
 // 此文件暂时为空，可以在未来添加交互功能
 console.log("欢迎来到许兵的个人网站！");
+
+// 跳转功能的实现
+document.getElementById('jump-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var url = document.getElementById('url-input').value;
+    if (url) {
+        if (!url.startsWith('http://') && !url.startsWith('https://')) {
+            url = 'http://' + url;
+        }
+        window.location.href = url;
+    } else {
+        alert('请输入一个有效的网址。');
+    }
+});
